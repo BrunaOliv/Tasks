2df2b078d32b9df3a6474e3a8965f522f8747afe
Bruna <brunasoaresoliv@outlook.com>
2025-04-09 19:52:10 -0300

    add protecao rota

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import styles from './sytles.module.css'
+import { GetServerSideProps } from 'next'
+import {getSession} from 'next-auth/react'
 
 export default function Dashboard(){
     return(
@@ -10,4 +12,23 @@ export default function Dashboard(){
             <h1>Pagina painel</h1>
         </div>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({req}) => {
+
+    const session = await getSession({req})
+
+    
+    if(!session?.user){
+        return{
+             redirect:{
+                destination: '/',
+                permanent: false
+             }
+        }
+    }
+
+    return{
+        props: {}
+    }
 }
