8d05f1b38e7ec55a8d96f7ebf858197b9a970322
Bruna <brunasoaresoliv@outlook.com>
2025-04-12 08:38:41 -0300

    armazenando e recuperando dados do firebase

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -5,22 +5,84 @@ import {getSession} from 'next-auth/react'
 import {Textarea} from '@/components/textarea'
 import {FiShare2} from 'react-icons/fi'
 import {FaTrash} from 'react-icons/fa'
-import { ChangeEvent, FormEvent, useState } from 'react'
+import { ChangeEvent, FormEvent, useState, useEffect } from 'react'
+import {db} from '@/services/firebaseConection'
+import {addDoc, collection, query, orderBy, where, onSnapshot, or} from 'firebase/firestore'
 
-export default function Dashboard(){
+
+interface HomeProps{
+    user:{
+        email: string;
+    }
+}
+
+interface TasksProps{
+    id: string,
+    created: Date,
+    public: boolean,
+    tarefa: string,
+    user: string
+}
+
+export default function Dashboard({user} : HomeProps){
 
     const [input, setInput] = useState('');
     const [publicTask, setPublicTask] = useState(false);
+    const [tasks, setTasks] = useState<TasksProps[]>([]);
+
+    useEffect(() => {
+        async function loadTarefas(){
+            const tarefasRef = collection(db, "tarefas")
+            const q = query(
+                tarefasRef,
+                orderBy("created", "desc"),
+                where("user", "==", user?.email )
+            )
+
+            onSnapshot(q, (snapshot) => {
+                let lista = [] as TasksProps[];
+                snapshot.forEach((doc) => {
+                    lista.push({
+                        id: doc.id,
+                        tarefa: doc.data().tarefa,
+                        created: doc.data().created,
+                        user: doc.data().user,
+                        public: doc.data().public
+                    })
+                })
+
+                setTasks(lista);
+            })
+        }
+
+        loadTarefas();
+    }, [user?.email]);
 
     function handleChangePublic(event: ChangeEvent<HTMLInputElement>){
         console.log(event.target.checked)
         setPublicTask(event.target.checked);
     }
 
-    function handleRegisterTask(event: FormEvent){
+    async function handleRegisterTask(event: FormEvent){
         event.preventDefault();
 
         if(input === "") return;
+
+        try{
+
+            await addDoc(collection(db, "tarefas"),{
+                tarefa: input,
+                created: new Date(),
+                user: user?.email,
+                public: publicTask
+            })
+
+            setInput("");
+            setPublicTask(false);
+
+        }catch(err){
+            console.log(err);
+        }
 
     }
 
@@ -63,21 +125,26 @@ export default function Dashboard(){
                 <section className={styles.tasksContainer}>
                     <h1>Minhas Tarefas</h1>
 
-                    <article className={styles.task}>
-                        <div className={styles.tagContainer}>
+                    {tasks.map((item) => (
+                        <article key={item.id} className={styles.task}>
+                            
+                        {item.public &&(
+                            <div className={styles.tagContainer}>
                             <label className={styles.tag}>PUBLICO</label>
                             <button className={styles.shareButton}>
                                 <FiShare2 size={22} color="#3183ff" />
                             </button>
                         </div>
+                        )}
 
                         <div className={styles.taskContent}>
-                            <p>Minha primeira tarefa de exmplo para o curso de Next</p>
+                            <p>{item.tarefa}</p>
                             <button className={styles.trashButton}>
                                 <FaTrash size={24} color='#ea3140'/>
                             </button>
                         </div>
                     </article>
+                    ))}
                 </section>
             </main>
         </div>
@@ -87,7 +154,6 @@ export default function Dashboard(){
 export const getServerSideProps: GetServerSideProps = async ({req}) => {
 
     const session = await getSession({req})
-
     
     if(!session?.user){
         return{
@@ -99,6 +165,10 @@ export const getServerSideProps: GetServerSideProps = async ({req}) => {
     }
 
     return{
-        props: {}
+        props: {
+            user:{
+                email: session?.user?.email
+            },
+        },
     }
 }
